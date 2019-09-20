f14dea9c9944e4b11066667471e68b816046287e
Luis Vargas <luisf55horn@gmail.com>
2019-09-20 15:26:58 -0400

    Insight Grid Layout

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import classes from '/resources/scss/custom.scss';
+
+
+if (module.hot) {
+    module.hot.dispose(function() {
+      // El módulo está a punto de ser reemplazado
+    })
+  
+    module.hot.accept(function() {
+      // El módulo o alguna de sus dependencias ya fueron actualizadas.
+    })
+  }
+
+/* */
